c34c84fe7f06aa7fd1b8bfb82c5d7d1c879bfec8
Shwetang <shwetangsingh5@gmail.com>
2024-02-22 21:56:46 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const port = process.env.PORT || 5000;
 
 // localhost-mongodb-connection-string: mongodb://localhost:27017/form
 
-mongoose
+mongoose 
     .connect("mongodb+srv://shwetangsingh:tpUaxdceEAQGEUK6@cluster0.qfsnwdk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     .then(() =>
         app.listen(port, () => console.log(`Listening at port ${port}...`))
